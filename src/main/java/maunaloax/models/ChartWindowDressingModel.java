ef9415761df12fc973076ad887e1268ff4cc4e77
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2014-02-03 23:25:24 +0100

    More scaffold.clj testing, some cleanup in MongoWindowDressingModel.clj (fetch/save methods)

--- src/main/java/maunaloax/models/ChartWindowDressingModel.java
+++ src/main/java/maunaloax/models/ChartWindowDressingModel.java
@@ -12,15 +12,12 @@ import java.util.List;
 public interface ChartWindowDressingModel {
     public static int MONGO_FIBONACCI = 1;
     public static int MONGO_LEVELS = 2;
-    public static int MONGO_ALL = 3;
     public static int ACTIVE = 1;
     public static int INACTIVE = 2;
     public static int BOTH = 3;
     List<DBObject> fetchComments(ObjectId id);
     WriteResult addComment(ObjectId id, String comment);
-    WriteResult updateCoord(int collection, ObjectId id, DBObject p1, DBObject p2);
-/*    MongoDBResult save(int collection, String ticker, long location, DBObject p1, DBObject p2);
-    List<DBObject> fetch(int collection, String ticker, long location, int activeSwitch, Date fromDate, Date toDate);*/
+    WriteResult updateCoord(ChartWindowsDressingContext ctx);
     MongoDBResult save(ChartWindowsDressingContext ctx);
     List<DBObject> fetch(ChartWindowsDressingContext ctx);
     boolean isCloud();
